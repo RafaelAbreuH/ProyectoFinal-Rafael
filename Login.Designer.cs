bf4ad810abc166795b324b40b8f55aaabc3d9013
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2019-07-20 16:58:51 -0400

    Entidades y Login Creadas

---
+++ Login.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ProyectoFinal_Rafael
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ClavemaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.Entrarbutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UsuarioTextBox = new System.Windows.Forms.TextBox();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ClavemaskedTextBox
+            // 
+            this.ClavemaskedTextBox.Location = new System.Drawing.Point(77, 69);
+            this.ClavemaskedTextBox.Name = "ClavemaskedTextBox";
+            this.ClavemaskedTextBox.PasswordChar = '*';
+            this.ClavemaskedTextBox.Size = new System.Drawing.Size(149, 20);
+            this.ClavemaskedTextBox.TabIndex = 10;
+            // 
+            // Entrarbutton
+            // 
+            this.Entrarbutton.Location = new System.Drawing.Point(93, 111);
+            this.Entrarbutton.Name = "Entrarbutton";
+            this.Entrarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Entrarbutton.TabIndex = 9;
+            this.Entrarbutton.Text = "Entrar";
+            this.Entrarbutton.UseVisualStyleBackColor = true;
+            this.Entrarbutton.Click += new System.EventHandler(this.Entrarbutton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Clave:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Usuario:";
+            // 
+            // UsuarioTextBox
+            // 
+            this.UsuarioTextBox.Location = new System.Drawing.Point(77, 43);
+            this.UsuarioTextBox.Name = "UsuarioTextBox";
+            this.UsuarioTextBox.Size = new System.Drawing.Size(149, 20);
+            this.UsuarioTextBox.TabIndex = 6;
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 146);
+            this.Controls.Add(this.ClavemaskedTextBox);
+            this.Controls.Add(this.Entrarbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UsuarioTextBox);
+            this.Name = "Login";
+            this.Text = "LOGIN";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox ClavemaskedTextBox;
+        private System.Windows.Forms.Button Entrarbutton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UsuarioTextBox;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
+
---
+++ Login.cs
@@ -0,0 +1,86 @@
+﻿using BLL;
+using Entidades;
+using ProyectoFinal_Rafael.UI.Consultas;
+using ProyectoFinal_Rafael.UI.Registros;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal_Rafael
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void CleanProvider()
+        {
+            errorProvider.Clear();
+        }
+
+        private void Entrarbutton_Click(object sender, EventArgs e)
+        {
+            int paso = 0;
+            Expression<Func<Usuarios, bool>> filtrar = x => true;
+            List<Usuarios> user = new List<Usuarios>();
+            RepositorioBase<Usuarios> db = new RepositorioBase<Usuarios>();
+
+            CleanProvider();
+            if (UsuarioTextBox.Text == string.Empty)
+            {
+                paso = 1;
+                errorProvider.SetError(UsuarioTextBox, "Incorrecto");
+
+            }
+            if (ClavemaskedTextBox.Text == string.Empty)
+            {
+                paso = 1;
+                errorProvider.SetError(ClavemaskedTextBox, "Incorrecto");
+
+            }
+            if (paso == 1)
+            {
+                MessageBox.Show("Campos Vacios!!");
+                return;
+            }
+            if ((UsuarioTextBox.Text == "Admin") && (ClavemaskedTextBox.Text == "0000"))
+            {
+                this.Hide();
+                MainForm ver = new MainForm();
+                ver.Show();
+            }
+            else
+            {
+                filtrar = t => t.Usuario.Equals(UsuarioTextBox.Text);
+                user = db.GetList(filtrar);
+
+                if (user.Exists(x => x.Nombre == UsuarioTextBox.Text) && user.Exists(x => x.Clave == ClavemaskedTextBox.Text))
+                {
+                    this.Hide();
+                    MainForm ver = new MainForm();
+                    ver.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Nombre de usuario o contraseña incorrecta!!");
+                    errorProvider.SetError(ClavemaskedTextBox, "Incorrecto");
+                    errorProvider.SetError(UsuarioTextBox, "Incorrecto");
+                }
+            }
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            ClavemaskedTextBox.PasswordChar = MostrarcheckBox1.Checked ? '\0' : '*';
+        }
+    }
+}
+
